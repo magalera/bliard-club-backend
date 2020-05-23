ed00e0a971aa3eb3c70313f87a9f94197c2d7463
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-05-23 23:10:33 +0200

    App files

---
+++ src/main/java/com/magalera/bilardclub/BilardClubApplication.java
@@ -0,0 +1,13 @@
+package com.magalera.bilardclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilardClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilardClubApplication.class, args);
+	}
+
+}
