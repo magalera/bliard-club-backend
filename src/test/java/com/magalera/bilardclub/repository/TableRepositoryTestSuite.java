90dcbb1e22bdb4f0fb028950497938d4b39e69b2
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-05-26 15:33:31 +0200

    Add testTableRepositorySave

--- src/test/java/com/magalera/bilardclub/repository/TableRepositoryTestSuite.java
+++ src/test/java/com/magalera/bilardclub/repository/TableRepositoryTestSuite.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
@@ -21,36 +19,18 @@ public class TableRepositoryTestSuite {
 
     @Test
     public void testTableRepositorySave() {
-/*
-        // Given
-        Table table = Table.builder().number(8).build();
-
-        // When
-        tableRepository.save(table);
-
-        // Then
-        Long id = table.getId();
-        Optional<Table> byId = tableRepository.findById(id);
-        Assert.assertTrue(byId.isPresent());
-
-        // CleanUp
-       tableRepository.deleteById(id);
-    }
-
-    @Test
-    public void testTableRepositoryfindByNumber() {
-
         // Given
         Table table = Table.builder().number(2).build();
 
         // When
         Table saved = tableRepository.save(table);
-        Table byNumber = tableRepository.findByNumber(saved.getNumber());
 
         // Then
-        Assert.assertEquals(Optional.of(2), Optional.of(byNumber.getNumber()));
+        Long id = saved.getId();
+        Optional<Table> actualTable = tableRepository.findById(id);
+        Assert.assertTrue(actualTable.isPresent());
 
         // CleanUp
-        tableRepository.deleteById(saved.getId());*/
+        tableRepository.deleteById(id);
     }
 }
